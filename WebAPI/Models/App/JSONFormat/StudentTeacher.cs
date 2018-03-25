5799b97dcec96efea80f0a8b57e00042307a5a81
igorlevk <igorl3009@gmail.com>
2018-03-25 22:06:50 +0300

    Dynamic school selection

---
+++ WebAPI/Models/App/JSONFormat/StudentTeacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models.App.JSONFormat
+{
+    public class StudentTeacher
+    {
+        public string userName { get; set; }
+        public string role { get; set; }
+        public int schoolId { get; set; }
+        public int grade { get; set; }
+    }
+}
---
+++ WebAPI/Models/App/JSONFormat/UserDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models.App.JSONFormat
+{
+    public class UserDetails
+    {
+        public string token { get; set; }
+        public string userName { get; set; }
+        public string name { get; set; }
+    }
+}
